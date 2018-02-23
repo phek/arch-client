525f95a0e7efd68846f75d9d06c5e57e7c5256a9
Philip Ekblom <philip.ekblom@gmail.com>
2018-02-23 06:48:24 +0100

    Added input fields

--- JavaClient/src/main/java/li/litech/javaclient/FXMLController.java
+++ JavaClient/src/main/java/li/litech/javaclient/FXMLController.java
@@ -3,12 +3,11 @@ package li.litech.javaclient;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -25,37 +24,45 @@ public class FXMLController implements Initializable {
     private int PORT = 3000;
 
     @FXML
-    private Label label;
+    private Label response;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
         try {
-            login("admin", "admin");
+            String uname = username.getText();
+            String pw = password.getText();
+            if (uname != "" && pw != "") {
+                login(uname, pw);
+            }
         } catch (IOException | JSONException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
     private void login(String username, String password) throws IOException, JSONException {
-        String payload = "data={"
+        String payload = "{"
                 + "\"username\": \"" + username + "\", "
-                + "\"password\": \"" + password + "\", "
+                + "\"password\": \"" + password + "\""
                 + "}";
-        StringEntity entity = new StringEntity(payload,
-                ContentType.APPLICATION_FORM_URLENCODED);
+        StringEntity entity = new StringEntity(payload, ContentType.APPLICATION_JSON);
 
         HttpClient httpClient = HttpClientBuilder.create().build();
         HttpPost request = new HttpPost("http://" + SERVER + ":" + PORT + "/authentication");
-        request.addHeader("content-type", "application/x-www-form-urlencoded");
         request.setEntity(entity);
 
-        HttpResponse response = httpClient.execute(request);
-        String json = IOUtils.toString(response.getEntity().getContent());
+        HttpResponse res = httpClient.execute(request);
+        String json = IOUtils.toString(res.getEntity().getContent());
         JSONObject object = new JSONObject(json);
         try {
-            System.out.println(object.getString("token"));
+            response.setText(object.getString("token"));
         } catch (JSONException ex) {
-            System.out.println(object.getString("error"));
+            response.setText(object.getString("error"));
         }
     }
 
